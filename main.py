7e9487f033b71ab3ca57b80d7a389755a8d29721
unknown <evanshui0@gmail.com>
2017-08-27 05:08:56 -0700

    implement click events

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pandas_datareader import data, wb
 import datetime
 from bokeh.plotting import figure, show, output_file, ColumnDataSource
 from bokeh.io import output_notebook
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, OpenURL, TapTool
 from bokeh.models.widgets import Panel, Tabs
 from datetime import date, timedelta
 from dateutil.relativedelta import *
@@ -18,7 +18,8 @@ def data_to_CDS(start_date):
     return source
 
 def plot(p, source):
-    p.line('date', 'price', source=source)
+    '''p.line('date', 'price', source=source)'''
+    p.circle('date', 'price', size=10, source=source)
 
 delta_7_days = date.today() + relativedelta(days=-7)
 delta_month = date.today() + relativedelta(months=-1)
@@ -31,7 +32,34 @@ date_titles = ["week", "month", "3 months", "6 months", "year", "5 years"]
 start = datetime.datetime(2016, 3, 1)
 source = None
 stock_ticker = "ATVI"
+month_url = str(1)
+day_url = str(1)
+year_url = str(2011)
+url = "https://www.google.com/"
+sources_list = [data_to_CDS(date) for date in dates]
+figures_list = [figure(x_axis_type='datetime', width=1500, height=400,
+           title=title) for title in date_titles]
+fig_source_tuple_list = zip(figures_list,sources_list)
+fig_date_tuple_list = zip(figures_list, date_titles)
+for fig, source in fig_source_tuple_list:
+    fig.add_tools(HoverTool(tooltips=[
+        ("date", "@date{%F}"),
+        ("Price", "$@price{0.2f}"),
+        ("index", "$index")
+    ],
+        formatters={
+            "date": "datetime"
+        },
+        mode="vline"
+    ))
+    plot(fig, source)
+    fig.select(type=TapTool).callback = OpenURL(url=url)
+tab_list = [Panel(child=fig, title=date_title) for fig, date_title in fig_date_tuple_list]
+tabs = Tabs(tabs=tab_list)
+output_file("python_analyzer.html")
+show(tabs)
 
+'''
 hover = HoverTool(tooltips=[
     ("date", "@date{%F}"),
     ("Price", "$@price{0.2f}"),
@@ -44,13 +72,10 @@ hover = HoverTool(tooltips=[
 )
 source1 = data_to_CDS(delta_7_days)
 source2 = data_to_CDS(delta_month)
-
 p1 = figure(x_axis_type='datetime', width=500, height=200,
            title="week")
 p2 = figure(x_axis_type='datetime', width=500, height=200,
             title="month")
-figures = [figure(x_axis_type='datetime', width=500, height=200,
-           title=title) for title in date_titles]
 
 p1.add_tools(HoverTool(tooltips=[
     ("date", "@date{%F}"),
@@ -77,10 +102,5 @@ plot(p1, source1)
 plot(p2, source2)
 tab1 = Panel(child=p1,title="week")
 tab2 = Panel(child=p2,title="month")
-
-sources = [data_to_CDS(date) for date in dates]
-source = data_to_CDS(datetime.datetime(2016,3,1))
-
 tabs = Tabs(tabs=[tab1, tab2])
-output_file("python_analyzer.html")
-show(tabs)
+'''
